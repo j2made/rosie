2bfc3029070cbe1f7e0ef935792f2bcd0e42aeb1
Shaun Baer <shaun.baer@gmail.com>
2015-12-17 10:34:36 -0500

    Moved some additional scripts. NOT TESTED.

--- brewconfig.sh
+++ brewconfig.sh
@@ -28,6 +28,7 @@ brew cleanup
 
 # Install Applications
 # --------------------
+#
 # Install Caskroom
 brew install caskroom/cask/brew-cask
 
@@ -42,10 +43,13 @@ apps=(
   sublime-text3
   sourcetree
   transmit
-  dropbox
   slack
+  dropbox
+  google-drive
   appcleaner
+  xtrafinder
   transmission
+  adobe-creative-cloud
 )
 
 # Install apps to /Applications
@@ -63,12 +67,49 @@ brew tap caskroom/fonts
 fonts=(
   font-inconsolata
   font-droid-sans-mono
+  font-dejavu-sans
+  font-montserrat
+  font-merriweather
 )
 
 # install fonts
 echo "installing fonts..."
 brew cask install ${fonts[@]}
 
-# Setup for NVM
+
+# Bash Profile
+# ------------
+#
+# Copy Bash Profile
+echo "setting up bash_profile..."
+cp -i .bash_profile ~/.bash_profile
+
+# Setup for NVM alias
 [[ -d ~/.nvm ]] || mkdir ~/.nvm
+
+# Setup for Sublime alias
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+
+# Reload .bash_profile
+source ~/.bash_profile 
+
+
+# Node.js and NPM
+# ---------------
+#
+# Install Node v5.0, set it to default
+echo "installing node.js..."
+nvm install v5.0 && nvm alias default v5.0
+
+# Install Gulp and Bower
+echo "installing npm modules..."
+npm install -g gulp && npm install -g bower && npm install -g browser-sync
+
+
+# Composer
+# --------
+echo "installing composer..."
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
 
